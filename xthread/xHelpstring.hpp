9501ec47e2486460d0e7d82f43af5a0c52307296
xjshi <xjshi@coremail.cn>
2018-12-14 18:09:49 +0800

    添加xHelpstring.hpp函数

--- xthread/xHelpstring.hpp
+++ xthread/xHelpstring.hpp
@@ -10,6 +10,16 @@
 7. 字符串分割
 8. 字符串替换
 
-
-
 */
+#pragma once
+#include <stdio.h>
+#include <string>
+//将字节流（二进制）转换成16机制数据。如果小于16，则在前面补‘0’
+inline static void bytesToHex(const void* pSrcBinary,unsigned int nSize,std::string &strRetHex)
+{
+
+}
+inline static void HexToBytes(const void pstrHex,unsigned int nSize,OUT std::string &strRetBytes)
+{
+
+}
